2ccfe1ab201b6de37b1e01470b86cc13664a4bdc
TarasFSD <ttarass09it@gmail.com>
2024-02-22 09:19:07 +0200

    done hw 1

---
+++ HW_1/index.js
@@ -0,0 +1,35 @@
+const a = "hello"
+const b = "owu"
+const c = "com"
+const e = "ua"
+const f = 1
+const g = 10
+const h = -999
+const i = 123
+const j = 3.14
+const k = 2.17
+const l = 16
+const m = true
+const n = false
+
+console.log(a, b, c, e, f, g, h, i, j, k, l, m, n)
+
+const firstName = "Тарас"
+const secondName = "Федерович"
+const lastName = "Болюбаш"
+
+let person = `${firstName}, ${secondName}, ${lastName}`
+console.log("person", person)
+
+let data1  = 100;
+let data2 = '100';
+let data3 = true;
+
+console.log(typeof data1)
+console.log(typeof data2)
+console.log(typeof data3)
+
+let userName = prompt("Please enter your name", "Harry Potter");
+
+
+
